0384a3949da97d203709d4ebdf5a10aa2ef1107b
sovello <sovellohpmgani@gmail.com>
2015-07-06 08:29:04 -0400

    adds __str__ method to models

--- person/models.py
+++ person/models.py
@@ -11,21 +11,26 @@ class CreateUpdate(models.Model):
         abstract = True
 
     
-
 class Gender(CreateUpdate):
     name = models.CharField(verbose_name = "Sex", max_length = 255)
 
     class meta:
         verbose_name_plural = 'gender/sex'
 
-        
+    def __str__(self):
+        return self.name
+
+    
 class MaritalStatus(CreateUpdate):
     name = models.CharField(max_length = 255)
 
     class Meta:
         verbose_name_plural = 'marital status'
 
-        
+    def __str__(self):
+        return self.name
+
+    
 class Person(CreateUpdate):
     first_name = models.CharField(max_length=255, verbose_name="First name")
     surname = models.CharField(max_length=255, verbose_name="Surname")
@@ -37,7 +42,10 @@ class Person(CreateUpdate):
         ordering = ['first_name', 'surname']
         verbose_name_plural = "persons"
 
-        
+    def __str__(self):
+        return "{} {} {}".format(self.first_name, self.surname, self.othername)
+
+    
 class PersonDemographic(CreateUpdate):
     gender = models.ForeignKey(Gender, blank = True)
     date_of_birth = models.DateField(blank = True)
@@ -47,7 +55,10 @@ class PersonDemographic(CreateUpdate):
     class Meta:
         verbose_name = 'demographic'
 
-        
+    def __str__(self):
+        return "{} {}".format(self.person, self.gender)
+
+    
 class PersonGuardian(CreateUpdate):
     father_name = models.CharField(max_length=255, blank=True, verbose_name="Father's Name")
     mother_name = models.CharField(max_length=255, blank=True, verbose_name="Mother's Name")
@@ -57,7 +68,10 @@ class PersonGuardian(CreateUpdate):
     class Meta:
         verbose_name = 'guardian/next of kin'
 
-        
+    def __str__(self):
+        return "{} {} {}".format(self.person, self.father_name, self.mother_name)
+
+    
 class PersonContacts(CreateUpdate):
     telephone = models.CharField(max_length=255, blank=True, verbose_name="Telephone (Mobile)")
     mailing_address = models.CharField(max_length=255, blank=True, verbose_name="Mailing Address")
@@ -66,6 +80,10 @@ class PersonContacts(CreateUpdate):
 
     class Meta:
         verbose_name='person contact'
+
+    def __str__(self):
+        return "{} {}".format(self.person, self.telephone)
+
     
 class PersonVitals(CreateUpdate):
     body_temperature = models.FloatField(blank=True, help_text="Temperature in Degrees Celcius")
@@ -75,3 +93,6 @@ class PersonVitals(CreateUpdate):
 
     class Meta:
         verbose_name = 'vital'
+
+    def __str__(self):
+        return "{} {}".format(self.person, self.body_temperature, self.weight)
